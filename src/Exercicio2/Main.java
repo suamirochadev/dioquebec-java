f9fbb3b0a8046b46ab91d7506d100658eec2ae74
SuamiRocha <suamirochati@gmail.com>
2022-10-11 09:53:58 -0300

    Exercicio 2 Sobrecarga

---
+++ src/Exercicio2/Main.java
@@ -0,0 +1,14 @@
+package Exercicio2;
+
+public class Main {
+    public  static void main(String[] args) {
+
+        //Quadrilátero
+        System.out.println("Exercício quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
