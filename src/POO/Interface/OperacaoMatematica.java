67c7417084d89341f37894568eb5a905eec4663a
SuamiRocha <suamirochati@gmail.com>
2022-10-17 17:36:54 -0300

    poo

---
+++ src/POO/Interface/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+package POO.Interface;
+
+interface OperacaoMatematica {
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+
+}
